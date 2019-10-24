e68c5d93b7b63cc3a7719d96fb71d4fa0a59f95c
Nessie-LQ <56944173+Nessie-LQ@users.noreply.github.com>
2019-10-24 14:58:17 +1100

    Add files via upload

---
+++ coursera-assignment-week1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Capstone Week 1
+
+# * This notebook is for the capstone of data science on coursera.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
